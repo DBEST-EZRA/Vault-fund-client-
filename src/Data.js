0ee261850ee6f5c9252e720028f9e77656180d17
DBEST-EZRA <ezradbest101@gmail.com>
2025-02-21 12:46:17 +0300

    second upper

---
+++ src/Data.js
@@ -0,0 +1,35 @@
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [
+      { list: "Home" },
+      { list: "About" },
+      { list: "Service" },
+      { list: "Property" },
+      { list: "Contact" },
+      { list: "Blog" },
+    ],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [
+      { list: "Headers" },
+      { list: "Features" },
+      { list: "Attractive" },
+      { list: "Testimonials" },
+      { list: "Videos" },
+      { list: "Footers" },
+    ],
+  },
+  {
+    title: "COMPANY",
+    text: [
+      { list: "About" },
+      { list: "Blog" },
+      { list: "Pricing" },
+      { list: "Affiliate" },
+      { list: "Login" },
+      { list: "Changelog" },
+    ],
+  },
+];
